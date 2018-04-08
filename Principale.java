40af77030d33ebaf460e7171e54a7f17621dd8c7
ALI <ali.belhadj@niji.fr>
2018-04-08 14:37:59 +0200

    instanciation de la classe Ville dans la classe Principale

---
+++ Principale.java
@@ -0,0 +1,9 @@
+package fr.openclassrooms.principale
+public class Principale{
+	public static void main (String[] args){
+		Ville ville1 = new Ville("Lyon","France",513 275 );
+		ville1.getNom();
+	}
+
+
+}
